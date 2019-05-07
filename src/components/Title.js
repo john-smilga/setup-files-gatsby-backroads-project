5aa6ef19af97c774ece9b7a5dbc99994cb8b5fb9
John <johnsmilga@gmail.com>
2019-05-06 18:32:16 -0700

    backroads setup files

---
+++ src/components/Title.js
@@ -0,0 +1,28 @@
+import React from "react"
+import styled from "styled-components"
+const Title = ({ title, subtitle, className }) => {
+  return (
+    <div className={className}>
+      <h4>
+        <span className="title">{title}</span>
+        <span>{subtitle}</span>
+      </h4>
+    </div>
+  )
+}
+
+export default styled(Title)`
+  text-transform: uppercase;
+  font-size: 2.3rem;
+  margin-bottom: 2rem;
+  h4 {
+    text-align: center;
+    position: relative;
+    letter-spacing: 7px;
+    color: var(--primaryColor);
+  }
+  .title {
+    color: var(--mainBlack);
+    margin-right: 1rem;
+  }
+`
